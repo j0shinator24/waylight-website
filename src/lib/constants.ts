4cb42963afeb3e67b401c899a8dbd2f5778c8549
Joshua <j0shinator24@users.noreply.github.com>
2026-04-05 22:29:32 +1000

    Auto-sync: 2026-04-05 22:29:32

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,7 @@ export const BUSINESS = {
   legalName: "Waylight Pty Ltd",
   abn: "49 689 498 350",
   email: "hello@waylightpm.com.au",
+  phone: "0451 306 669",
   location: "Queensland, Australia",
   founder: "Joshua",
   registrationGroup: "0127",
